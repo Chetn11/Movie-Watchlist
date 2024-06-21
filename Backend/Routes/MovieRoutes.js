3423e2931f5a11b9b84900596ddf9668081a1fc2
Chetn11 <cnpatil899@gmail.com>
2024-06-21 10:25:37 +0530

    backend done

--- Backend/Routes/MovieRoutes.js
+++ Backend/Routes/MovieRoutes.js
@@ -8,15 +8,40 @@ moviesRouter.get("/",async(req,res)=>{
     res.send({data:data});
 })
 
+
+// creating Data
 moviesRouter.post("/add",async(req,res)=>{
     const{title,image,year,genre,rating,description,status}=req.body;
     
     try {
         const moviesData=await MoviesModel.create({title,image,year,genre,rating,description,status});
-    res.send({data:moviesData,message:"Data Added to the server"})
+    res.send({data:moviesData,message:`${moviesData.title} is added`})
     } catch (error) {
         res.send({message:"Error while adding data"})
     }
     
+})
+
+// Edit Data
+moviesRouter.post("/edit/:movieId", async(req,res)=>{
+    const movieId=req.params.movieId;
+    const payload=req.body
+    try {
+        const movieData=await MoviesModel.findOneAndUpdate({_id:movieId},payload);
+        res.send({data:movieData,message:`${moviesData.title} is updated`})
+    } catch (error) {
+        res.send({message:"Error while Editing data"})
+    }
+})
+
+// delete Data
+moviesRouter.delete("/delete/:movieId", async(req,res)=>{
+    const movieId=req.params.movieId;
+    try {
+        const movieData=await MoviesModel.findOneAndDelete({_id:movieId});
+        res.send({message:`${movieData.title} is deleted`})
+    } catch (error) {
+        res.send({message:"Error while Deleting data"})
+    }
 })
 module.exports={moviesRouter}
